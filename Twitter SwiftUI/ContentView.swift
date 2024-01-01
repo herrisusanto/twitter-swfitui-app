9eb3a883e5c4262f5f293670a56dec92ecc69267
herisusanto <herisusanto.app@gmail.com>
2024-01-01 21:33:57 +0700

    Initial Commit

---
+++ Twitter SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Twitter SwiftUI
+//
+//  Created by loratech on 01/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Twitter SwiftUI/Twitter_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_SwiftUIApp.swift
+//  Twitter SwiftUI
+//
+//  Created by loratech on 01/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
